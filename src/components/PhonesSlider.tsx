8d25fbf0e7894a88fac081171e64058860dadad7
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-08-04 20:01:51 +0300

    did responsive swiper

--- src/components/PhonesSlider.tsx
+++ src/components/PhonesSlider.tsx
@@ -16,30 +16,42 @@ export const PhonesSlider = <T extends unknown>({
   children,
   topLineButtons,
   spaceBetween,
-}:
-IPhonesSlider<T>) => {
+}: IPhonesSlider<T>) => {
   const refContainer = useRef<HTMLDivElement>(null);
   const refList = useRef<HTMLUListElement>(null);
 
   const [currentPosition, setCurrentPosition] = useState(0);
-  const cardWidth = (refList?.current?.children?.[0] as HTMLElement)
-    .offsetWidth;
+
+  // const breakpoints = {
+  //   large: 1024,
+  //   medium: 768,
+  //   small: 0,
+  // };
+
   const containerWidth = refContainer?.current?.clientWidth;
+  const cardElement = refList?.current?.children?.[0] as
+    | HTMLElement
+    | undefined;
+  const cardWidth = cardElement?.offsetWidth ?? 0;
   const scrollWidth = refList?.current?.scrollWidth;
 
   const handleNextClick = () => {
     if (currentPosition > -((scrollWidth ?? 0) - (containerWidth ?? 0))) {
-      setCurrentPosition((prevPosition) => (
-        prevPosition - cardWidth) - spaceBetween);
+      setCurrentPosition(
+        (prevPosition) => prevPosition - cardWidth - spaceBetween,
+      );
     }
   };
 
   const handlePrevClick = () => {
     if (currentPosition < 0) {
-      setCurrentPosition((prevPosition) => (
-        prevPosition + cardWidth) + spaceBetween);
+      setCurrentPosition(
+        (prevPosition) => prevPosition + cardWidth + spaceBetween,
+      );
     }
   };
+
+  // eslint-disable-next-line @typescript-eslint/no-shadow
 
   return (
     <>
@@ -54,7 +66,12 @@ IPhonesSlider<T>) => {
         >
           {items.map((item, index) => (
             // eslint-disable-next-line react/no-array-index-key
-            <Fragment key={index}>
+            <Fragment
+              // style={{ width: `${cardWidth}px`, marginRight: `${spaceBetween}px` }}
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+            >
+
               {typeof children === 'function' ? children(item) : children}
             </Fragment>
           ))}
